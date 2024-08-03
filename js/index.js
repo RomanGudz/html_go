407c0160f5aac0bcb68a4a82a5ba45dadc5db9b5
Roman Gudz <alien31roman@gmail.com>
2024-08-03 23:29:02 +0300

    module_04_lessons_06

--- js/index.js
+++ js/index.js
@@ -25,3 +25,40 @@ modal.on('click', function (e) {
   }
 
 })
+
+
+
+const accordion = $('.accordoin');
+
+accordion.accordion({
+  active: true,
+  collapsible: true,
+  icons: {
+    header: 'acc__icon-white',
+    activeHeader: 'acc__icon-white acc__icon-white-active',
+  }
+});
+
+
+const h3 = document.querySelectorAll('.faq__item-h3-btn');
+h3.forEach(elem => {
+  const colorIcon = elem.dataset.iconColor;
+  const span = elem.querySelector('.ui-icon');
+  if (!colorIcon) {
+    span.classList.remove('acc__icon-white')
+    span.classList.add('acc__icon')
+  }
+});
+
+ymaps.ready(init);
+
+function init() {
+  const myMap = new ymaps.Map("map", {
+    center: [55.723661504206355, 37.56504308465882],
+    zoom: 17
+  });
+
+  const mark = new ymaps.Placemark([55.723661504206355, 37.56504308465882]);
+
+  myMap.geoObjects.add(mark);
+}
