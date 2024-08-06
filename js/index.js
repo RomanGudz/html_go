e40bd971db03cf11481c7a6a9bfa7e10d01cda11
Roman Gudz <alien31roman@gmail.com>
2024-08-06 23:55:55 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -34,8 +34,8 @@ accordion.accordion({
   active: true,
   collapsible: true,
   icons: {
-    header: 'acc__icon-white',
-    activeHeader: 'acc__icon-white acc__icon-white-active',
+    // header: 'acc__icon-white',
+    // activeHeader: 'acc__icon-white acc__icon-white-active',
   }
 });
 
@@ -44,10 +44,32 @@ const h3 = document.querySelectorAll('.faq__item-h3-btn');
 h3.forEach(elem => {
   const colorIcon = elem.dataset.iconColor;
   const span = elem.querySelector('.ui-icon');
+  const li = elem.closest('li');
+  span.classList.add('acc__icon-white')
   if (!colorIcon) {
-    span.classList.remove('acc__icon-white')
-    span.classList.add('acc__icon')
+    span.classList.toggle('acc__icon-white');
+    span.classList.toggle('acc__icon');
   }
+
+  li.addEventListener('click', (e) => {
+    const target = e.target;
+    const span = target.querySelector('.acc__icon');
+    const spanWhite = target.querySelector('.acc__icon-white');
+    if (span) {
+      span.classList.toggle('acc__icon-active');
+    }
+    if (spanWhite) {
+      spanWhite.classList.toggle('acc__icon-white-active');
+    }
+  });
+
+  span.addEventListener('click', (e) => {
+    e.target.classList.toggle('acc__icon-white-active');
+    if (!colorIcon) {
+      e.target.classList.toggle('acc__icon-active');
+      e.target.classList.toggle('acc__icon-white-active');
+    }
+  })
 });
 
 ymaps.ready(init);
